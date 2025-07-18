bcf23568ba4ed42fa4b37e9598f7cc966f9bf85a
Fuuu <derickfernandosilva2007@gmail.com>
2025-07-18 05:35:14 -0300

    Organização de pastas
    Por questões de organização fiz questão de organizar e separar algumas funções em 2 scripts separados, para ter uma workspace mais clean visualmente!

---
+++ Projetos Em C#/Soluções Especificas/Sistema De jogos que já joguei/GamesContainer/Principal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Sistema_De_jogos_que_já_joguei.GamesContainer
+{
+    internal class Principal
+    {
+        static void Main(string[] args)
+        {
+
+            GamesOrganizer gamesOrganizer = new GamesOrganizer();
+            Principal principal = new Principal();
+
+            gamesOrganizer.LoadGames();
+
+            Console.WriteLine("Bem vindo ao seu organizador de Games, aqui estão os seu jogos atualmente zerados:");
+            foreach (var Game in gamesOrganizer.Games)
+            {
+                Console.WriteLine(Game);
+            }
+            if (gamesOrganizer.Games.Count <= 0)
+            {
+                Console.WriteLine("zerou nada dog");
+            }
+            Console.WriteLine("\nGostaria de adicionar mais algum ou remover?\nEscreva 'adicionar' ou 'remover'\ncaso " +
+                "queria somente ver os games denovo escreva 'Mostrar' ");
+            var resposta = Console.ReadLine();
+            bool isInvalid;
+            switch (resposta.ToLower())
+            {
+                case "adicionar": gamesOrganizer.AddGame(); break;
+                case "remover": gamesOrganizer.RemoveGame(); break;
+                case "mostrar": gamesOrganizer.displaygames(); break;
+
+
+            }
+        }    
+    }
+
+
+
+}
