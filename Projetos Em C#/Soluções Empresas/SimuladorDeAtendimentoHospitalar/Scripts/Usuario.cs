f524fdcb6de56e571fc57f2b83a17dd8431b2ebc
Fuuu <derickfernandosilva2007@gmail.com>
2025-07-24 05:01:15 -0300

    Initial commit
    Fix algumas classes para começar o projeto, fiz algumas modificações no projeto para separar o .cs principal(Nesse caso o Visual) dos outros .cs que servirão de lógica.
    Mais commits serão feitas conforme atualizações mais importantes.

---
+++ Projetos Em C#/Soluções Empresas/SimuladorDeAtendimentoHospitalar/Scripts/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorDeAtendimentoHospitalar
+{
+    /// <summary>
+    /// Todos os metodos que vem de user pode ser chamados pelo processo especifico
+    /// </summary>
+    public  class Usuario
+    {
+       Usuario user = new Usuario();
+       private string user_Login;
+       private string user_Password;
+       public string User_Login { get { return user_Login; } set { User_Login = value; } }
+       public string User_Password { get { return User_Password; } set { User_Password = value; } }
+        public virtual void test()
+        {
+
+        }
+   
+
+    }
+
+    public class Login : Usuario
+    {
+        
+    }
+
+    public class Sing_in : Usuario 
+    {
+   
+    }
+    public class Admin : Usuario
+    {
+    }
+
+}
---
+++ Projetos Em C#/Soluções Empresas/SimuladorDeAtendimentoHospitalar/Scripts/Visual.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimuladorDeAtendimentoHospitalar
+{
+    internal class Visual
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Usuario usuario = new Usuario();
+            Sing_in sing_In = new Sing_in();
+            sing_In.test();
+        
+
+           
+            
+        }
+    }
+}
